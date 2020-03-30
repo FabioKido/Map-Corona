d3d298757851dec3c42121ccc865c6204c743965
FabioKido <fabiohenryquemesquita@gmail.com>
2020-03-30 14:12:41 -0300

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import { Link } from 'gatsby';
+
+const Index = () => {
+	return (
+		<div>
+			<p>Olá Mundo!</p>
+		</div>
+	);
+}
+
+export default Index;
---
+++ src/pages/pt-br.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PT_BR = () => {
+	return (
+		<h1>Página em PT-BR</h1>
+	);
+}
+
+export default PT_BR;
